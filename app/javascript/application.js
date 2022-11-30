cb8f0a938d6c586ef6cf2826b219874222a24b62
shaqayq <shaqayq.darwazi@gmail.com>
2022-11-30 16:48:48 +0430

    add files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
